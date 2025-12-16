4cb298212d0701c7698902b85fd996320e336674
Devanshu Sharma <devanshu11122005@gmail.com>
2025-12-17 01:43:27 +0530

    Seller Dashboard Add in Payment Service with broker

--- seller-dashboard/src/broker/listener.js
+++ seller-dashboard/src/broker/listener.js
@@ -2,6 +2,7 @@ const { subscribeToQueue } = require("./broker");
 const userModel = require("../models/user.model");
 const productModel = require("../models/product.model")
 const orderModel = require("../models/order.model")
+const paymentModel = require("../models/payment.model")
 
 module.exports = async function () {
 
@@ -17,13 +18,13 @@ module.exports = async function () {
         await orderModel.create(order)
     })
 
-    // subscribeToQueue("PAYMENT_SELLER_DASHBOARD.PAYMENT_CREATED", async (payment) => {
-    //     await paymentModel.create(payment)
-    // })
+    subscribeToQueue("PAYMENT_SELLER_DASHBOARD.PAYMENT_CREATED", async (payment) => {
+        await paymentModel.create(payment)
+    })
 
-    // subscribeToQueue("PAYMENT_SELLER_DASHBOARD.PAYMENT_UPDATE", async (payment) => {
-    //     await paymentModel.findOneAndUpdate({ orderId: payment.orderId }, { ...payment })
-    // })
+    subscribeToQueue("PAYMENT_SELLER_DASHBOARD.PAYMENT_UPDATE", async (payment) => {
+        await paymentModel.findOneAndUpdate({ orderId: payment.orderId }, { ...payment })
+    })
 
 
 
